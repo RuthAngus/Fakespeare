12584d3cd046f7ee21fb9f000b9e8595a18fc791
Dan F-M <danfm@nyu.edu>
2016-06-20 15:22:19 +0000

    better sampling

--- sample.py
+++ sample.py
@@ -3,11 +3,7 @@
 
 from __future__ import division, print_function
 
-import os
-import sys
-import glob
 import json
-import random
 import argparse
 import numpy as np
 
@@ -17,6 +13,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("arch", help="the architecture JSON")
 parser.add_argument("maps", help="the mappers JSON")
 parser.add_argument("weights", help="the weights JSON")
+parser.add_argument("-o", "--output", default="sample.txt",
+                    help="the output sample file")
+parser.add_argument("-n", "--nchars", default=1000, type=int,
+                    help="the total number of characters to generate")
 args = parser.parse_args()
 
 with open(args.arch, "r") as f:
@@ -35,56 +35,27 @@ def sample(a, temperature=1.0):
     a = np.exp(a) / np.sum(np.exp(a))
     return np.argmax(np.random.multinomial(1, a, 1))
 
-sentence = "\n\n**CHAR0**\n\nDark was the night as sometimes whatever"[:maxlen]
-print(len(sentence))
+random_seed = input("Random seed: ")
+if len(random_seed):
+    np.random.seed(int(random_seed))
+seed = input("Seed dialog: ")
+
+sentence = "**CHAR0**\n\n" + seed
 generated = "" + sentence
-for i in range(100):
+sentence = sentence[-maxlen:]
+if len(sentence) < maxlen:
+    sentence = ("\n" * (maxlen - len(sentence))) + sentence
+for i in range(args.nchars):
     x = np.zeros((1, maxlen, len(char_indices)))
     for t, char in enumerate(sentence):
         x[0, t, char_indices[char]] = 1.
 
     preds = model.predict(x, verbose=0)[0]
-    next_index = sample(preds, 1.0)
+    next_index = sample(preds, 0.5)
     next_char = indices_char[next_index]
     generated += next_char
     sentence = sentence[1:] + next_char
 
+with open(args.output, "w") as f:
+    f.write(generated)
 print(generated)
-
-# # train the model, output generated text after each iteration
-# for iteration in range(1, 60):
-#     print()
-#     print('-' * 50)
-#     print('Iteration', iteration)
-#     model.fit(X, y, batch_size=128, nb_epoch=1)
-
-#     model.save_weights(os.path.join(outdir,
-#                                     'weights_{0:05d}.h5'.format(iteration)))
-
-#     start_index = random.randint(0, len(text) - maxlen - 1)
-
-#     for diversity in [0.2, 0.5, 1.0, 1.2]:
-#         print()
-#         print('----- diversity:', diversity)
-
-#         generated = ''
-#         sentence = text[start_index: start_index + maxlen]
-#         generated += sentence
-#         print('----- Generating with seed: "' + sentence + '"')
-#         sys.stdout.write(generated)
-
-#         for i in range(400):
-#             x = np.zeros((1, maxlen, len(chars)))
-#             for t, char in enumerate(sentence):
-#                 x[0, t, char_indices[char]] = 1.
-
-#             preds = model.predict(x, verbose=0)[0]
-#             next_index = sample(preds, diversity)
-#             next_char = indices_char[next_index]
-
-#             generated += next_char
-#             sentence = sentence[1:] + next_char
-
-#             sys.stdout.write(next_char)
-#             sys.stdout.flush()
-#         print()
